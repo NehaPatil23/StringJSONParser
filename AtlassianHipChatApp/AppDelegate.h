3d784b4bb080fde0fab593aff157493a992b6694
NehaPatil23 <nehapatil@enmacbook-ap.local>
2016-04-27 11:59:03 +1000

    Atlassian Hip Chat Application used for parsing string into JSON

--- AtlassianHipChatApp/AppDelegate.h
+++ AtlassianHipChatApp/AppDelegate.h
@@ -3,7 +3,7 @@
 //  AtlassianHipChatApp
 //
 //  Created by Neha Patil on 22/04/2016.
-//  Copyright © 2016 University. All rights reserved.
+//  Copyright © 2016 University. All rights reserved. 
 //
 
 #import <UIKit/UIKit.h>
